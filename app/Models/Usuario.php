607c4c2dc25dde65f3efaf165aac0ab6fc59fea1
RyanRigoto <164576343+RyanRIGOTO@users.noreply.github.com>
2025-10-31 01:15:51 +0000

    Corrige model Usuario: remove tag PHP duplicada e limpa arquivo

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -1,13 +1,21 @@
+<?php
+// ...existing code...
+<?php
+
+namespace App\Models;
+
 use Illuminate\Database\Eloquent\Model;
 
-class Funcionario extends Model
+class Usuario extends Model
 {
+    protected $table = 'usuarios';
+
     protected $fillable = [
-	'nome',
-    'cpf',
-    'telefone',
-    'endereco',
-	'email',
-	'senha',
-	]
+        'nome',
+        'cpf',
+        'telefone',
+        'endereco',
+        'email',
+        'senha',
+    ];
 }
